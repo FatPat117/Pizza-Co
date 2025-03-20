56a077063e4debc10993679e8ee3d0bedaa4341a
Phat117 <ntphat110705@gmail.com>
2025-03-20 14:30:23 +0700

    Setting up Tailwind CSS

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -1,15 +1,15 @@
-import { Link } from "react-router-dom";
-import SearchOrder from "../features/order/SearchOrder";
+import { Link } from 'react-router-dom';
+import SearchOrder from '../features/order/SearchOrder';
 
 function Header() {
-        return (
-                <header>
-                        <Link to="/">Fast Pizza Co.</Link>
+  return (
+    <header>
+      <Link to="/">Fast Pizza Co.</Link>
 
-                        <SearchOrder />
-                        <p>PITACHITI </p>
-                </header>
-        );
+      <SearchOrder />
+      <p>PITACHITI </p>
+    </header>
+  );
 }
 
 export default Header;
