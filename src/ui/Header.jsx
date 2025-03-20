b70a7f55b79dcaff5e4f7ecd34a255f18d56068e
Phat117 <ntphat110705@gmail.com>
2025-03-20 21:24:15 +0700

    Styling Buttons: Element States and Transitions

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -4,7 +4,7 @@ import UserName from '../features/users/UseerName';
 
 function Header() {
   return (
-    <header className="flex items-center justify-between border-b-2 border-stone-300 bg-yellow-500 px-4 py-3 uppercase sm:px-6">
+    <header className="flex items-center justify-between border-b-2 border-stone-300 bg-yellow-400 px-4 py-3 uppercase sm:px-6">
       <Link to="/" className="tracking-widest">
         Fast Pizza Co.
       </Link>
